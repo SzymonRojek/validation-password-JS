f5da63c3db73926aad5f1d7586478a0a7f0ff30e
Szymon Rojek <sz.rojek@gmail.com>
2022-03-25 19:32:23 +0000

    create index.js and export functions

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+export { default as countNumbers } from "./countNumbers";
+export { default as countSpecialCharacters } from "./countSpecialCharacters";
+export { default as countUpperLetters } from "./countUpperLetters";
+export { default as hasNumber } from "./hasNumber";
+export { default as hasSpecialCharacter } from "./hasSpecialCharacter";
+export { default as hasUpperLetter } from "./hasUpperLetter";
+export { default as hasProperLength } from "./hasProperLength";
+export { default as isNotFunctionError } from "./isNotFunctionError";
+export { default as isNotStringError } from "./isNotStringError";
+export { default as passwordScore } from "./passwordScore";

1ff1325719623e006f4cfc6d513a88963c797fbc
Szymon Rojek <sz.rojek@gmail.com>
2022-03-28 20:12:01 +0100

    increase length

--- src/helpers/hasProperLength.js
+++ src/helpers/hasProperLength.js
@@ -1,7 +1,9 @@
-const lengthError = { message: "The length should have minimum 5 characters!" };
+const lengthError = {
+  message: "The length should have min 5 & max 12 characters!",
+};
 
 export default function hasProperLength(password) {
-  if (password.length < 5 || password.length > 10) {
+  if (password.length < 5 || password.length > 12) {
     return lengthError;
   }
 

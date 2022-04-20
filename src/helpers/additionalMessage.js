f59c296ed7faabe8a1bb37b12d003e8011172bb8
Szymon Rojek <sz.rojek@gmail.com>
2022-04-20 21:44:38 +0100

    clean code

--- src/helpers/additionalMessage.js
+++ src/helpers/additionalMessage.js
@@ -1,9 +1,9 @@
 function additionalMessage(message) {
-    const additionalScoreElement = document.querySelector(
-        ".js-additionalMessage"
-    );
+  const additionalScoreElement = document.querySelector(
+    ".js-additionalMessage"
+  );
 
-    additionalScoreElement.innerHTML = `${message}`;
+  additionalScoreElement.innerHTML = `${message}`;
 }
 
 export default additionalMessage;
